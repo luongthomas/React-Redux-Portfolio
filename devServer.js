3819813d80e100864870ef34a0d3b06f67d5a587
luongthomas <luongt@oregonstate.edu>
2016-11-15 13:35:44 -0800

    changed port

--- devServer.js
+++ devServer.js
@@ -17,11 +17,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', (err) => {
+let port = process.env.PORT || 3000;
+
+app.listen(port, 'localhost', (err) => {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://localhost:' + port);
 });
