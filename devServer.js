5d3ec8bf04394047aab3878edae47e5feee3e870
luongthomas <luongt@oregonstate.edu>
2016-11-15 19:35:07 -0800

    changed default port

--- devServer.js
+++ devServer.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7070;
 
 app.listen(port, 'localhost', (err) => {
   if (err) {
