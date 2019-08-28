67d0ce9e094e084d0684760b58b1dc15c13629d1
Thomas Luong <Luongt@me.com>
2019-08-27 22:23:03 -0700

    Update personal info and resume

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -1,13 +1,13 @@
 const personalInfoData = {
   name: 'Thomas Luong',
   greeting: "Hi, I'm Thomas.",
-  subGreeting: "I'm an iOS and a web developer.",
+  subGreeting: "I'm a Software Developer.",
   miniIntro: "I love bringing ideas to life through programming.  My favorite tools are Python, JavaScript and Swift.  You can find me taking pictures around the Portland area.",
   linkedIn: 'https://www.linkedin.com/in/luongthomas',
   github: 'https://github.com/luongthomas',
   angelList: 'https://angel.co/jay-chen-5',
   email: 'mailto:luongt@oregonstate.edu',
-  resume: 'https://a.pomf.cat/ornona.pdf'
+  resume: 'https://website-bucket-thomas-luong.s3-us-west-2.amazonaws.com/thomas_resume2019.pdf'
 };
 
 export { personalInfoData };
